cec81b1aea6d5f0794e1359bca3b2e0b1e4be574
Phu <phuktvn0@gmail.com>
2022-10-28 19:34:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
+const api = {
+  key: '321c531125e17deb15ecc3632bbd0d57',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+document.title = 'Weather';
+
+
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchCity, setSearchCity] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErroMessage] = useState(false);
+  const [weatherInfor, setWeatherInfor] = useState('');
+
+useEffect(() =>{
+  const dataWeather = async () => {
+    if (!searchCity) return;
+    setLoading(true);
+    try {
+      
+      const response = await fetch(`${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`);
+      console.log(`${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`);
+      const data = await response.json();
+      if (response.ok) {
+        setWeatherInfor(
+          `${data.name}, ${data.sys.country}, ${data.weather[0].description}, ${data.main.temp}`
+        );
+      } else {
+        setErroMessage(data.message)
+        setErroMessage('');
+      }
+    } catch (error) {
+      setErroMessage(error.message);
+    };
+    setLoading(false);
+  }
+  dataWeather();
+},[searchCity])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setSearchCity(searchInput);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text" 
+      placeholder='City' 
+      value={searchInput} 
+      onChange={(e)=>setSearchInput(e.target.value)}
+      />
+      <button>Search</button>
+    </form>
+    {loading ? (<div>Loading...</div>
+    ) : (
+      <>
+        {errorMessage ? (
+            <div style={{color:'red'}}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfor}</div>) }
+        </>
+      )}
+    </>
   );
 }
 
